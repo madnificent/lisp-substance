dca8d96013df5f63ff589a98cb5bd9cf026a24a1
Eric Bergstrome <eric.bergstrome@gmail.com>
2011-04-01 18:41:03 -0300

    Use exec for connect.bash tail call

--- connect.bash
+++ connect.bash
@@ -6,4 +6,4 @@ palm-package -o /tmp $ADDICT_DIR
 palm-install $ADDICT_IPK
 echo will launch $ADDICT in one second
 (sleep 1; palm-launch $ADDICT) >/dev/null 2>&1 &
-bin/server -a $ADDICT_IP_ADDR
+exec bin/server -a $ADDICT_IP_ADDR
