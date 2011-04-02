25d8e9e361720723a1aa0a7aa367484390540eb3
Eric Bergstrome <eric.bergstrome@gmail.com>
2011-04-02 19:23:32 -0300

    Only connect.bash refers to /tmp

--- connect.bash
+++ connect.bash
@@ -3,7 +3,7 @@
 . src/config.bash
 
 palm-package -o /tmp $ADDICT_DIR
-palm-install $ADDICT_IPK
+palm-install /tmp/$ADDICT_IPK
 echo will launch $ADDICT in one second
 (sleep 1; palm-launch $ADDICT) >/dev/null 2>&1 &
 exec bin/server -a $ADDICT_IP_ADDR
